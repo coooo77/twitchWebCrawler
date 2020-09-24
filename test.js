8eb72ab97f983dfb68a1222be9cefdeb00669343
TL Chang <coooo77@hotmail.com>
2020-09-24 15:41:33 +0800

    [新增] 資料庫MongoDB

--- test.js
+++ test.js
@@ -16,6 +16,21 @@ const config = {
   }
 }
 
+// 連線資料庫MongoDB
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/puppeteer', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const puppeteer = require('puppeteer-core');
 
 
